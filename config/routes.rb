fc4698791e6b814d7300b9f1178d28e1084971e6
Lindsey Johnson <redhook@hotmail.com>
2017-12-20 15:04:32 -0700

    heroku ready

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   resources :sessions, except: [:destroy]
   delete '/sessions' => 'sessions#destroy', as: :logout
   resources :comments
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'home#index'  
 end
